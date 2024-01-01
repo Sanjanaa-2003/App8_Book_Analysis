6cf2c5da1a59752836864a4a3061fb2d35d123be
Sanjanaa R J <149648708+Sanjanaa-2003@users.noreply.github.com>
2024-01-01 17:18:55 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+
+
+
+
+
+# #Open anaconda prompt
+# Enter the directory where the python project is saved.
+# eg: e:
+# type jupyter lab
+# tHIS will open jupyter lab in the respective drive and intetgate it with pycharm
